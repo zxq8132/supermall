65cb43a3fbd53642423d18ec39e90b530e33d111
unknown <zxqsoft@126.com>
2020-09-15 10:04:38 +0800

    15/9

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,21 @@
+<!-- 组件说明home页的特征部分 -->
+<template>
+  <div class="feature">
+    <a href="#">
+      <img src="~assets/img/home/recommend_bg.jpg" alt />
+    </a>
+  </div>
+</template>
+
+<script>
+//import x from ''
+export default {
+  name: "FeatureView",
+};
+</script>
+
+<style  scoped>
+.feature img {
+  width: 100%;
+}
+</style>
